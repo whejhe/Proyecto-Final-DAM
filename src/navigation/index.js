f16c6c58fc31f886db5ec1a13c837098dbba4a34
Carlos <dominguezalacid@gmail.com>
2025-02-25 12:11:03 +0100

    actualizar

---
+++ src/navigation/index.js
@@ -0,0 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
